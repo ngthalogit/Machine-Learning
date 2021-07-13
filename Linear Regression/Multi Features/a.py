80f985cf6eda57f9eecb17109f6d1d1501701c77
Nguyễn Thành Long <83007241+ngthalogit@users.noreply.github.com>
2021-07-13 16:34:43 +0700

    Add files via upload

---
+++ Linear Regression/Multi Features/a.py
@@ -0,0 +1,26 @@
+"""
+
+auth: Nguyen Thanh Long
+
+"""
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+
+if __name__ == '__main__':
+
+    filename = '/home/ntl0601/Downloads/Commit for git/kc_house_data.csv'
+    data = pd.read_csv(filename)[0:500]
+
+    y = np.array(data['price'].values)
+    data = data.drop(['date', 'price', 'id'], axis=1)
+    X = np.array(data.values)
+
+    bias = np.ones((1, len(X)))
+    X_bias = np.concatenate((X, bias.T), axis=1)
+
+    regr = linear_model.LinearRegression()
+    regr.fit(X_bias, y)
+    W = [regr.coef_[:-1], regr.intercept_]
+    
+    print(W)
