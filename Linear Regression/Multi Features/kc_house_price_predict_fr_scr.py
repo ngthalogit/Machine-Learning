1c4c8f1fde0ab1530b614934e7359b4968f53b21
Nguyễn Thành Long <83007241+ngthalogit@users.noreply.github.com>
2021-07-13 20:03:22 +0700

    Update kc_house_price_predict_fr_scr.py

--- Linear Regression/Multi Features/kc_house_price_predict_fr_scr.py
+++ Linear Regression/Multi Features/kc_house_price_predict_fr_scr.py
@@ -1,12 +1,14 @@
 """
 
 auth: Nguyen Thanh Long
+This program implement linear regression algorithm to pridect house's price.
 
 """
+# import Library
 import numpy as np
 import pandas as pd
 
-
+# calculate
 def fit(X, y):
     XX = np.linalg.pinv(np.dot(X, X.T))
     XXX = np.dot(XX, X)
@@ -17,12 +19,13 @@ def fit(X, y):
 if __name__ == '__main__':
 
     filename = 'kc_house_data.csv'
-    data = pd.read_csv(filename)[0:500]
+    data = pd.read_csv(filename)[0:500] # load data from .csv file, we just take the first 499 samples to calculate
 
-    y = np.array(data['price'].values)
+    y = np.array(data['price'].values) # house's price
     data = data.drop(['date', 'price', 'id'], axis=1)
-    X = np.array(data.values)
-
+    X = np.array(data.values) # 19 features includes 'bedrooms', 'bathrooms',...
+    
+    # add bias
     bias = np.ones((1, len(X)))
     X_bias = np.concatenate((X, bias.T), axis=1)
     
