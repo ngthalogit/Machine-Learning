3f9fdf62b9703f07f141d9fb628822e3785bb7e2
Nguyễn Thành Long <83007241+ngthalogit@users.noreply.github.com>
2021-07-13 21:01:54 +0700

    Update kc_house_price_predict_sklearn.py

--- Linear Regression/Multi Features/kc_house_price_predict_sklearn.py
+++ Linear Regression/Multi Features/kc_house_price_predict_sklearn.py
@@ -1,17 +1,15 @@
 """
-
 auth: Nguyen Thanh Long
-
 """
 
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 if __name__ == '__main__':
-
-    filename = 'kc_house_data.csv'
+    filename = '/home/ntl0601/Downloads/Commit for git/kc_house_data.csv'
     data = pd.read_csv(filename)[0:500]
 
     y = np.array(data['price'].values)
@@ -21,9 +19,22 @@ if __name__ == '__main__':
     bias = np.ones((1, len(X)))
     X_bias = np.concatenate((X, bias.T), axis=1)
 
+    X_train, X_test, y_train, y_test = train_test_split(X_bias, y, test_size=0.2, random_state=10)  # split X, y into
+    # train and test set
+
     regr = linear_model.LinearRegression()
-    regr.fit(X_bias, y)
-    
+    regr.fit(X_train, y_train)
+
+    # get weight
     W = [regr.coef_[:-1], regr.intercept_]
-    
-    print(W)
+
+    # predict
+    y_pre = regr.predict(X_test)
+
+    # evaluate model
+
+    mse = mean_squared_error(y_test, y_pre)  # Mean Square Error
+
+    rmse = np.sqrt(mse)  # Root Mean Square Error
+
+
