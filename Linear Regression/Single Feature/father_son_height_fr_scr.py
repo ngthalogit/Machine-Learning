4c79bf9ba0207f2362fe35b4be0000f1f9271168
Nguyễn Thành Long <83007241+ngthalogit@users.noreply.github.com>
2021-07-13 19:50:45 +0700

    Update father_son_height_fr_scr.py

--- Linear Regression/Single Feature/father_son_height_fr_scr.py
+++ Linear Regression/Single Feature/father_son_height_fr_scr.py
@@ -2,18 +2,26 @@
 
 auth: Nguyen Thanh Long
 
+This is program implement linear regression algorithm from scratch to predict height'son. 
+
 """
 
+# import Library
 import numpy as np
 
-
+#calculate 
 def fit(X_bias, y):
     XX = np.linalg.pinv(np.dot(X_bias, X_bias.T))
     XXX = np.dot(XX, X_bias)
     w = np.dot(XXX.T, y)
     return w
 
-
+# Read data from .txt file
+"""
+    File includes 1087 rows and 2 columns:
+        The first columns is height's father
+        The second columns is height's son 
+"""
 def init(file):
     father, son = [], []
     data = open(file, 'r').readlines()
@@ -33,9 +41,10 @@ if __name__ == '__main__':
     X = np.array([height[0]]).T
     y = np.array(height[1])
     
+    # add bias 
     bias = np.ones((1, len(X)))
     X_bias = np.concatenate((X, bias.T), axis=1)
     
     W = fit(X_bias, y)
     
-    print(W)
+    print(W) 
