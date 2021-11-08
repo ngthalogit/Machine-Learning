c7ffbac1da68860ac66d17f46c6cbd69fe704563
Nguyen Thanh Long <ngthlo.doc@gmail.com>
2021-11-08 16:32:33 +0700

    ''

---
+++ Convex Optimization/simplex.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# init data
+obj_func = np.array([[-5, -3]], dtype=float) # objective function
+G = np.array([[1, 1, 1, 0, 0],
+              [2, 1, 0, 1, 0],
+              [1, 4, 0, 0 ,1]], dtype=float)
+h = np.array([[10, 16, 32]], dtype=float).T
+
+
+# TODO : solve this problem with simplex method
+
+
+
+
+
